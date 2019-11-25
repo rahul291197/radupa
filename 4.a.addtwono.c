46058c171364d3289ec72569bd3aeb4d966792b5
rahul291197 <58158641+rahul291197@users.noreply.github.com>
2019-11-25 10:10:44 +0530

    Add files via upload

---
+++ 4.a.addtwono.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+/*
+
+*/
+void main()
+{
+  int fn, sn, sum;
+  float average;
+
+printf("enter first number:");
+scanf("%d",&fn);
+printf("enter second number");
+scanf("%d",&sn);
+
+sum=fn+sn;
+average=sum/2  ;
+
+ printf("sum = %d, average = %f",sum,average);
+   getch();
+
+}
